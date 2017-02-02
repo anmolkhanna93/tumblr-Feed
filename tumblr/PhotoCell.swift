afac196d63eceef90879d7ed29ee25a2315df073
anmolkhanna93 <anmolkhanna93@gmail.com>
2017-02-01 22:16:17 -0500

    Tumblr Feed
    A simple applications which displays photos in Feed from Tumblr

---
+++ tumblr/PhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCell.swift
+//  tumblr
+//
+//  Created by Anmol Khanna on 2/1/17.
+//  Copyright © 2017 Anmol Khanna. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var pcell: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
